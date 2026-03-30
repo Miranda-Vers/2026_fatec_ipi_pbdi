bc0d54bec3e35072dd4428737930ab2a32e09c1a
Miranda-vers <arthursilvamiranda@gmail.com>
2026-03-30 18:08:29 -0300

    Readme e menu

---
+++ 2026_fatec_ipi_pbdi/README.md
@@ -0,0 +1,4 @@
+# 2026_fatec_ipi_pbdi
+Nome: Arthur Silva Miranda
+RA: 2041382611039
+
---
+++ 2026_fatec_ipi_pbdi/menu.py
@@ -0,0 +1,21 @@
+#1.2 Crie um arquivo chamado menu.py.
+#1.3 Implemente uma função chamada menu que ofereça as seguintes opções ao usuário:
+#1. Somar
+#2. Subtrair
+#3. Multiplicar
+#4 . Dividir
+#0. Sair
+
+
+
+print("Boa tarde, quais das opções deseja escolher? ")
+print("1. Somar")
+print("2. Subtrair")
+print("3. Multiplicar")
+print("4. Dividir")
+print("0. Sair do programa")
+
+
+    
+
+
