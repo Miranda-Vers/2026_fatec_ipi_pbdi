b4b3326ce236c4d86ce2cbf3f9cd2b12cde78618
Miranda-vers <arthursilvamiranda@gmail.com>
2026-03-30 20:52:59 -0300

    Calculo subtração

--- 2026_fatec_ipi_pbdi/menu.py
+++ 2026_fatec_ipi_pbdi/menu.py
@@ -9,7 +9,7 @@
 
 def menu():
 
-    print("Boa tarde, quais das opções deseja escolher? ")
+    print("Boa tarde, quais das opções deseja escolher?  ")
     print("1. Somar")
     print("2. Subtrair")
     print("3. Multiplicar")
@@ -19,15 +19,20 @@ def menu():
 escolha_usuario = int(input("Digite a opção desejada:"))
 if escolha_usuario == 1:
     print("Você escolheu a opção de somar")
+    n1= float(input("Digite o primeiro número: "))
+    n2= float(input("Digite o segundo número: "))
+    print(f"A soma dos números é {n1 + n2}")
 elif escolha_usuario == 2:
     print("Você escolheu a opção de subtrair")
+    n1= float(input("Digite o primeiro número: "))
+    n2= float(input("Digite o segundo número: "))
+    print(f"A subtração dos números é {n1 - n2}")
 elif escolha_usuario == 3:
     print("Você escolheu a opção de multiplicar")
 elif escolha_usuario == 4:
     print("Você escolheu a opção de dividir")
 elif escolha_usuario == 0:
     print("Você escolheu a opção de sair do programa")
-else:    print("Opção inválida, por favor escolha uma opção válida")
+else:
+    print("Opção inválida, por favor escolha uma opção válida")
 
-
-menu()
