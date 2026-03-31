7be288473cf7284689cedefcaa61dc6ffca227fd
Miranda-vers <arthursilvamiranda@gmail.com>
2026-03-30 21:04:19 -0300

    Finalizado: Chamada da função menu adicionada

--- 2026_fatec_ipi_pbdi/menu.py
+++ 2026_fatec_ipi_pbdi/menu.py
@@ -6,9 +6,7 @@
 #4 . Dividir
 #0. Sair
 
-
 def menu():
-
     print("Boa tarde, quais das opções deseja escolher?  ")
     print("1. Somar")
     print("2. Subtrair")
@@ -16,31 +14,32 @@ def menu():
     print("4. Dividir")
     print("0. Sair do programa")
 
-escolha_usuario = int(input("Digite a opção desejada:"))
-if escolha_usuario == 1:
-    print("Você escolheu a opção de somar")
-    n1= float(input("Digite o primeiro número: "))
-    n2= float(input("Digite o segundo número: "))
-    print(f"A soma dos números é {n1 + n2}")
-elif escolha_usuario == 2:
-    print("Você escolheu a opção de subtrair")
-    n1= float(input("Digite o primeiro número: "))
-    n2= float(input("Digite o segundo número: "))
-    print(f"A subtração dos números é {n1 - n2}")
-elif escolha_usuario == 3:
-    print("Você escolheu a opção de multiplicar")
-    n1= float(input("Digite o primeiro número: "))
-    n2= float(input("Digite o segundo número: "))
-    print(f"A multipicação dos números é {n1 * n2}")
-elif escolha_usuario == 4:
-    print("Você escolheu a opção de dividir")
-    n1= float(input("Digite o primeiro número: "))
-    n2= float(input("Digite o segundo número: "))
-    print(f"A divisão dos números é {n1 / n2}")
-    
-elif escolha_usuario == 0:
-    print("Você escolheu a opção de sair do programa")
-    
-else:
-    print("Opção inválida, por favor escolha uma opção válida")
+    escolha_usuario = int(input("Digite a opção desejada:"))
+    if escolha_usuario == 1:
+        print("Você escolheu a opção de somar")
+        n1= float(input("Digite o primeiro número: "))
+        n2= float(input("Digite o segundo número: "))
+        print(f"A soma dos números é {n1 + n2}")
+    elif escolha_usuario == 2:
+        print("Você escolheu a opção de subtrair")
+        n1= float(input("Digite o primeiro número: "))
+        n2= float(input("Digite o segundo número: "))
+        print(f"A subtração dos números é {n1 - n2}")
+    elif escolha_usuario == 3:
+        print("Você escolheu a opção de multiplicar")
+        n1= float(input("Digite o primeiro número: "))
+        n2= float(input("Digite o segundo número: "))
+        print(f"A multipicação dos números é {n1 * n2}")
+    elif escolha_usuario == 4:
+        print("Você escolheu a opção de dividir")
+        n1= float(input("Digite o primeiro número: "))
+        n2= float(input("Digite o segundo número: "))
+        print(f"A divisão dos números é {n1 / n2}")
+    elif escolha_usuario == 0:
+        print("Você escolheu a opção de sair do programa, até mais!")
+    else:
+        print("Opção inválida, por favor escolha uma opção válida")
+
+
+menu()
 
